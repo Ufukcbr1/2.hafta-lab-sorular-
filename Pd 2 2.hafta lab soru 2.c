dc9e50aedc2fb8bc2f9c9bfce0745847af21e818
Ufukcbr1 <105659986+Ufukcbr1@users.noreply.github.com>
2022-05-17 13:57:02 +0300

    Add files via upload

---
+++ Pd 2 2.hafta lab soru 2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+  void fonk(void);
+  
+  int gid = 287;
+  
+  int main(void){
+  	printf("deneme1.c gid degisken degeri: %d\n", gid);
+  	
+  	fonk();
+  	return 0;
+	
+   }
+ 
+   extern int gid;
+   
+   void fonk(void){
+   	printf("deneme2.c gid degisken degeri: %d", gid); 
+   	
+   }
