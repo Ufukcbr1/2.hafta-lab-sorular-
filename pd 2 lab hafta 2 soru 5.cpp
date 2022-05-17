4bdfa5aa0da978ebbffc62de8a778574553b57d6
Ufukcbr1 <105659986+Ufukcbr1@users.noreply.github.com>
2022-05-17 14:02:04 +0300

    Add files via upload

---
+++ pd 2 lab hafta 2 soru 5.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+    float square(const int r){
+    	
+  	    float Alan;
+  	    const float pi = 3.14;
+  	    Alan = pi * r * r;     
+		  
+		return alan; }
+    
+    
+	
+	int main (){
+    	
+  	int r;
+  	
+  	printf("Lütfen bir yaricap degeri giriniz:");
+  	scanf("%d", &r);
+  	
+  	printf("alani=%.2f", square(r));
+    
+  	
+  	
+  	 } 
